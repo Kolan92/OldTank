6fc609cabeca0a8049ab639a114b425c0ab1cad7
Pawel Kolanowski <pawel.krzysztof.kolanowski@gmail.com>
2016-06-02 18:31:50 +0200

    Init

---
+++ Assets/Complete/Scripts/GameController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Complete {
+    public class GameController : MonoBehaviour {
+
+        //public GameData GameData;
+        public static GameController InstanceGameController = null;
+    }
+}
